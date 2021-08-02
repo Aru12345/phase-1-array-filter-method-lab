090fe0c4904aa3998fcb90f544093c16276a872b
Arundhati <arundhatiraicar12@gmail.com>
2021-08-03 00:27:49 +0530

    commit tree 64aa0c1142018c75e921ef7c417ab8769b153874

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+/*function findMatching(drivers,string){
+ for(const elements of drivers){
+   if(elements ===string){
+     return string.toUpperCase()
+   }
+ }
+}*/
+function findMatching(names,string){
+   return names.filter(match=>match.toLowerCase()===string.toLowerCase())
+}
+function fuzzyMatch(names,string){
+  return names.filter(letter=>letter.charAt(0)===string.charAt(0))
+}
+function matchName(names,string){
+  return names.filter(nameProperty=>nameProperty.name===string)
+}
